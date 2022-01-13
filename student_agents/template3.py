62724eaf95fd3ddf44226f422e3aa9a2b7c9517b
Timo Luebbing <timo.luebbing@gmail.com>
2022-01-13 18:44:28 +0100

    util functions

--- student_agents/template3.py
+++ student_agents/template3.py
@@ -77,6 +77,7 @@ def howManyPiecesLost(gs: ChessEngine.GameState, is_white_turn):
 
     return white - black if is_white_turn else black - white
 
+
 def isChecked(gs: ChessEngine.GameState, is_white_turn):
     checkExists = gs.checkForPinsAndChecks()[0]
     if checkExists:
@@ -84,6 +85,48 @@ def isChecked(gs: ChessEngine.GameState, is_white_turn):
     else:
         return 0
 
+
+def movePawnsForward(gs: ChessEngine.GameState, is_white_turn):
+    # How far player "white" pawns are
+    board = gs.board
+    whiteAhead = 0
+    blackAhead = 0
+    for key, piece in enumerate(board):
+        if piece == "bp":
+            howFarAhead = getRow(key)
+            blackAhead += howFarAhead
+        elif piece == "wp":
+            howFarAhead = 5 - getRow(key)
+            whiteAhead += howFarAhead
+    return whiteAhead if is_white_turn else blackAhead
+
+def getRow(positionArray):
+    return positionArray // 6
+
+
+def central_pieces(gs: ChessEngine.GameState, is_white_turn):
+    white = 0
+    black = 0
+    board = gs.board
+
+    central_squares       = [board[i] for i in [14, 15, 20, 21]]
+    outer_central_squares = [board[i] for i in [13, 16, 19, 22]]
+
+    for piece in central_squares:
+        if piece[0] == 'w':
+            white += 0.1
+        elif piece[0] == 'b':
+            black += 0.1
+    for piece in outer_central_squares:
+        if piece[0] == 'w':
+            white += 0.05
+        elif piece[0] == 'b':
+            black += 0.05
+    
+    return white - black if is_white_turn else black - white
+
+def castle_util(move, is_white_turn):
+    pass
 
 def is_start_game(gs):
     board = gs.board
@@ -102,15 +145,19 @@ def is_end_game(gs):
     return len([piece in special_pieces for piece in white_pieces]) <= 2 or len(
        [piece in special_pieces for piece in black_pieces]) <= 2
 
-def utility(gs, is_max_turn, move_to_current_gs = None):
+
+def utility(gs, is_white_turn, move_to_current_gs = None):
     if is_start_game(gs):
-        # print("is startgame")
-        pass
+        return howManyPiecesLost(gs, is_white_turn) + \
+               central_pieces(gs, is_white_turn)
+
     if is_end_game(gs):
-        # print("is endgame!!!!")
-        pass
+        return howManyPiecesLost(gs, is_white_turn) + \
+               0.01 * movePawnsForward(gs, is_white_turn) + \
+               isChecked(gs, is_white_turn)
     
-    return howManyPiecesLost(gs, is_max_turn) + isChecked(gs, is_max_turn)
+    return howManyPiecesLost(gs, is_white_turn) + \
+           isChecked(gs, is_white_turn)
 
 
 
